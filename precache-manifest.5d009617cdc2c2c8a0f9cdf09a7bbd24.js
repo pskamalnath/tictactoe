33ba6791b048c67d07eee6907c77c8cf562b946f
Parchuri, Sreenivas <kamalmca@gmail.com>
2020-06-09 16:29:49 +0530

    Updates

---
+++ precache-manifest.5d009617cdc2c2c8a0f9cdf09a7bbd24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61845cb330a8337c27af72c7cb414a11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70921f954933cd4a4aad",
+    "url": "/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "32540ac14b0f9e67caa5",
+    "url": "/static/js/2.ac17b5d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ac17b5d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70921f954933cd4a4aad",
+    "url": "/static/js/main.18ca118a.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
